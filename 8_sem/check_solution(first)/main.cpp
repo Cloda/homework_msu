d614371cdbabadb4442c5cbe546438abfac051f9
Cloda <icherepaxin@bk.ru>
2024-03-19 09:46:05 +0300

    Finish first homework

--- 8_sem/check_solution(first)/main.cpp
+++ 8_sem/check_solution(first)/main.cpp
@@ -3,18 +3,18 @@
 
 double y_k(int k, int m, int N)
 {
-    return sin((M_PI * (2*(double)m - 1) * ((double)k)) / ((double)N - 0.5));
+    return sin((M_PI * (2*m - 1) * (N - k)) / (2*N - 1));
 }
 
 double lambda_m(double p, int m, int N)
 {
-    return -p + 4*((double)N - 0.5)*((double)N - 0.5)*sin(M_PI*(2*(double)m - 1)/(4*(double)N - 2))*sin(M_PI*(2*(double)m - 1)/(4*(double)N - 2));
+    return (2*N - 1)*(2*N - 1)*sin((M_PI*(2*m - 1))/(4*N - 2))*sin((M_PI*(2*m - 1))/(4*N - 2));
 }
 
 int main()
 {
     int N = 10;
-    double p = 0.;
+    double p = 0.1;
     double h = 1. / ((double)N - 0.5);
 
     double lambda = 0.;
@@ -27,11 +27,10 @@ int main()
 
         for (int k = 1; k < N; k++)
         {
-            value_right = (y_k(k + 1, m, N) - 2 * y_k(k, m, N) + y_k(k - 1, m, N)) / (h * h) + p * y_k(k, m, N);
+            value_right = (y_k(k + 1, m, N) - 2 * y_k(k, m, N) + y_k(k - 1, m, N)) / (h * h);
             value_left = -lambda * y_k(k, m, N);
-            std::cout << value_right << " !------! " << value_left << std::endl;
 
-            if (fabs(value_right - value_left) > 1e-5)
+            if (fabs(value_right - value_left) > 1e-7)
             {
                 std::cout << "Fail" << std::endl;
                 break;
